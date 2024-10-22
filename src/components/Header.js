cf89c2fd7fd457bb3ae31ce49e0036b5cb2eb69c
actjacob <yakupaktar99@gmail.com>
2024-10-22 17:03:58 +0300

    Reactmemo()

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import { View, Text, StyleSheet } from 'react-native';
+import React from 'react';
+
+const Header = ({ count }) => {
+  console.log('Header component re-render');
+
+  const arr = new Array(count).fill().map(() => Math.floor(Math.random() * 10));
+  return (
+    <View style={styles.container}>
+      <Text>Header</Text>
+      <Text>{JSON.stringify(arr)} </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginBottom: 60,
+    borderBottomWidth: 2,
+    alignItems: 'center'
+  }
+});
+
+export default React.memo(Header);
